65a65338280db4a2d36eb6c789f1a1a587990e63
pablo <pablo@radius.net.br>
2020-09-28 21:25:38 -0300

    Primeira versao da listagem, mais adicionar biblioteca de icones

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg'
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <h1>Dashboard</h1>
-}
+    return (
+        <>
+            <img src={logoImg} alt="GitHub Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/13985639?s=460&u=72abfecb7269a45f3c1fb0e74e235eda92dc7191&v=4"
+                        alt="Pablo Marcondes"
+                    />
+
+                    <div>
+                        <strong>gitExplore</strong>
+                        <p>This project was bootstrapped with Create React App.</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/13985639?s=460&u=72abfecb7269a45f3c1fb0e74e235eda92dc7191&v=4"
+                        alt="Pablo Marcondes"
+                    />
+
+                    <div>
+                        <strong>gitExplore</strong>
+                        <p>This project was bootstrapped with Create React App.</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/13985639?s=460&u=72abfecb7269a45f3c1fb0e74e235eda92dc7191&v=4"
+                        alt="Pablo Marcondes"
+                    />
+
+                    <div>
+                        <strong>gitExplore</strong>
+                        <p>This project was bootstrapped with Create React App.</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
+};
 
 export default Dashboard;
